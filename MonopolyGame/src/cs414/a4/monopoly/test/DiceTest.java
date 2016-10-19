e108bf8f8aa16948e82c05251b4d909f1db571ca
ajcopley <ajcopley@rams.colostate.edu>
2016-10-18 19:54:33 -0600

    Fixed a quick attribute error in Dice.java, implemented diceTest.

--- MonopolyGame/src/cs414/a4/monopoly/test/DiceTest.java
+++ MonopolyGame/src/cs414/a4/monopoly/test/DiceTest.java
@@ -9,18 +9,32 @@ import org.junit.Test;
 import cs414.a4.monopoly.backEnd.*;
 
 public class DiceTest {
-
+	
+	private Dice d1;
+	private int die;
+	private boolean result;
+	
 	@Before
 	public void setUp() throws Exception {
+		d1 = new Dice();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		d1 = null;
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void diceValues1to6Test() {
+		for(int i = 0; i < 100; i++){
+			die = d1.rollDie();
+			if(die < 1 || die > 6){
+				result = false;
+			}
+			else{
+				result = true;
+			}
+			assertTrue(result);
+		}
 	}
-
 }
