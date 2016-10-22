30dd4d0fe7ef0d4da0c88f9c081956363fe20d1e
bhamor <ben.hamor94@gmail.com>
2016-10-21 21:42:27 -0600

    Added owner attribute to Property with appropriate getter and setter.

--- MonopolyGame/src/cs414/a4/monopoly/backEnd/Property.java
+++ MonopolyGame/src/cs414/a4/monopoly/backEnd/Property.java
@@ -8,12 +8,15 @@ public abstract class Property {
 	private int mortgageValue;
 	private boolean isMortgaged;
 	
-	public Property(String n, int c, int r, int m) {
+	Owner myOwner;
+	
+	public Property(String n, int c, int r, int m, Owner bank) {
 		name = n;
 		cost = c;
 		rent = r;
 		mortgageValue = m;
 		isMortgaged = false;
+		myOwner = bank;
 	}
 	
 	public String getName(){
@@ -38,6 +41,14 @@ public abstract class Property {
 	
 	public void setIsMortgaged(boolean m){
 		isMortgaged = m;
+	}
+	
+	public Owner getOwner(){
+		return myOwner;
+	}
+	
+	public void setOwner(Owner newOwner){
+		myOwner = newOwner;
 	}
 	
 	public abstract boolean mortgage();
