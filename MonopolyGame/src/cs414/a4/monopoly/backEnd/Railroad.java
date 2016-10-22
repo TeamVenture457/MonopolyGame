13db14ecbf0bda0fdabce5292ce4d8fab38338cc
bhamor <ben.hamor94@gmail.com>
2016-10-21 21:49:00 -0600

    added bank to constructors

--- MonopolyGame/src/cs414/a4/monopoly/backEnd/Railroad.java
+++ MonopolyGame/src/cs414/a4/monopoly/backEnd/Railroad.java
@@ -2,8 +2,8 @@ package cs414.a4.monopoly.backEnd;
 
 public class Railroad extends Property{
 
-	public Railroad(String n, int c, int r, int m){
-		super(n, c, r, m);
+	public Railroad(String n, int c, int r, int m, Owner bank){
+		super(n, c, r, m, bank);
 	}
 	
 	public boolean mortgage(){
--- MonopolyGame/src/cs414/a4/monopoly/backEnd/Utility.java
+++ MonopolyGame/src/cs414/a4/monopoly/backEnd/Utility.java
@@ -2,8 +2,8 @@ package cs414.a4.monopoly.backEnd;
 
 public class Utility extends Property{
 
-	public Utility(String n, int c, int r, int m){
-		super(n, c, r, m);
+	public Utility(String n, int c, int r, int m, Owner bank){
+		super(n, c, r, m, bank);
 	}
 	
 	public boolean mortgage(){
