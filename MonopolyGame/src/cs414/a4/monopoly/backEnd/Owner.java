763c237b310445e88e55a0b96bc7acf35a5a1adf
TatroIII <elitatro@mail.com>
2016-10-22 22:13:32 -0600

    Finished Owner Player Bank added systemMessages

--- MonopolyGame/src/cs414/a4/monopoly/backEnd/Owner.java
+++ MonopolyGame/src/cs414/a4/monopoly/backEnd/Owner.java
@@ -6,22 +6,4 @@ import java.util.List;
 public abstract class Owner {
     SystemMessagesToPlayer systemMessages;
 	protected List<Property> propertiesOwned;
-
-	public Owner() {
-		this.propertiesOwned = new ArrayList<Property>();
-	}
-
-    public List<Property> getPropertiesOwned(){
-        return this.propertiesOwned;
-    }
-
-	public abstract void buyHouse(Property property);
-    public abstract void buyHotel(Property property);
-    public abstract void morgage(Property property);
-    public abstract Property buyProperty(Property property);
-    public abstract Property sellProperty(Property property);
-    public abstract void sellHouse(Property property);
-    public abstract void sellHotels(Property property);
-
-
 }
--- MonopolyGame/src/cs414/a4/monopoly/backEnd/Player.java
+++ MonopolyGame/src/cs414/a4/monopoly/backEnd/Player.java
@@ -1,48 +1,174 @@
 package cs414.a4.monopoly.backEnd;
 
+import javax.swing.plaf.synth.SynthEditorPaneUI;
+import java.util.ArrayList;
+
 public class Player extends Owner{
-	
+
+
 	private String name;
 	private String token;
-	private Space location;
+	private int location;
 	private boolean isInJail;
+	private int money;
+	private Bank bank;
 
 	public Player(String name) {
 		this.name = name;
+		bank = Bank.getInstance();
+		this.money = 1500;
+		this.location = 0;
+		this.isInJail = false;
+		propertiesOwned = new ArrayList<Property>();
 	}
 
-	@Override
 	public void buyHouse(Street street){
-		Bank.getInstance().buyHouse();
+		if(bank.removeHouseFromBank()) {
+			if(getMoney()>=street.getHouseCost()) {
+				if(!street.placeHouse()){
+					SystemMessagesToPlayer.getSystemMessage(6);
+					return;
+				}else{
+					this.removeMoney(street.getHouseCost());
+					return;
+				}
+			}else{
+				SystemMessagesToPlayer.getSystemMessage(8);
+				return;
+			}
+		}else{
+			return;
+		}
+
 	}
 
-	@Override
-	public void buyHotel(Property property) {
-
+	public void buyHotel(Street street) {
+		if(bank.removeHotelFromBank()) {
+			if(getMoney()>=street.getHotelCost()) {
+				if(!street.placeHotel()){
+					SystemMessagesToPlayer.getSystemMessage(7);
+					return;
+				}else{
+					this.removeMoney(street.getHotelCost());
+					return;
+				}
+			}else{
+				SystemMessagesToPlayer.getSystemMessage(8);
+				return;
+			}
+		}else{
+			return;
+		}
 	}
 
-	@Override
 	public void morgage(Property property) {
 
-	}
+		if(propertiesOwned.contains(property)){
+			if(bank.mortgage(property)) {
+				addMoney(property.getMortgageValue());
+				return;
+			}else{
+				SystemMessagesToPlayer.getSystemMessage(9);
+			}
 
-	@Override
-	public Property buyProperty(Property property) {
-		return null;
-	}
-
-	@Override
-	public Property sellProperty(Property property) {
-		return null;
-	}
-
-	@Override
-	public void sellHouse(Property property) {
+		}
 
 	}
 
-	@Override
-	public void sellHotels(Property property) {
+	public void addProperty(Property property, int cost) {
+		if(getMoney()>=cost) {
+			propertiesOwned.add(property);
+			this.money = this.money - cost;
+		}else{
+			SystemMessagesToPlayer.getSystemMessage(10);
+		}
+	}
+
+	public void addProperty(Property property) {
+		if (!propertiesOwned.contains(property)){
+			propertiesOwned.add(property);
+		}
+	}
+
+	public void removeProperty(Property property, int price) {
+		if(propertiesOwned.contains(property)) {
+			propertiesOwned.remove(property);
+			this.money = this.money +price;
+		}else{
+			SystemMessagesToPlayer.getSystemMessage(11);
+		}
+	}
+
+	public void removeProperty(Property property) {
+		if(propertiesOwned.contains(property)) {
+			propertiesOwned.remove(property);
+		}else{
+			SystemMessagesToPlayer.getSystemMessage(11);
+		}
+	}
+
+	public void sellHouse(Street street) {
+		if(street.removeHouse()){
+			this.money = this.money + street.getHouseCost()/2;
+			bank.addHouseToBank();
+		}else{
+			SystemMessagesToPlayer.getSystemMessage(12);
+		}
+	}
+
+	public void sellHotels(Street street) {
+		if(street.removeHotel()){
+			this.money = this.money + street.getHotelCost()/2;
+			bank.addHotelToBank();
+		}else{
+			SystemMessagesToPlayer.getSystemMessage(13);
+		}
+	}
+
+	public void removeMoney(int amount){
+		this.money = this.money-amount;
+	}
+
+	public void addMoney(int amount){
+		this.money = this.money+amount;
+	}
+
+	public int getMoney(){
+		return this.money;
+	}
+
+	public boolean getIsInJail(){
+		return this.isInJail;
+	}
+
+	public void setIsInJail(boolean bool){
+		this.isInJail=bool;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+
+	public String getToken(){
+		return this.token;
+	}
+
+	public void setToken(String token){
+		this.token = token;
+	}
+
+	public int getLocation(){
+		return this.location;
+	}
+
+	public void putInJail(){
+		this.location=40;
+		setIsInJail(true);
+	}
+
+	public void takeOutofJail(){
+		this.location=10;
+		setIsInJail(false);
 
 	}
 
