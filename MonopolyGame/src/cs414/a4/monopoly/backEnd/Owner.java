ad3061d5d00aa8cdb70cde261bfe9253287a1ba9
TatroIII <elitatro@mail.com>
2016-10-22 19:50:52 -0600

    Proper bank methods

--- MonopolyGame/src/cs414/a4/monopoly/backEnd/Owner.java
+++ MonopolyGame/src/cs414/a4/monopoly/backEnd/Owner.java
@@ -1,11 +1,27 @@
 package cs414.a4.monopoly.backEnd;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Owner {
-	
-	private int money;
+    SystemMessagesToPlayer systemMessages;
+	protected List<Property> propertiesOwned;
 
 	public Owner() {
-		// TODO Auto-generated constructor stub
+		this.propertiesOwned = new ArrayList<Property>();
 	}
+
+    public List<Property> getPropertiesOwned(){
+        return this.propertiesOwned;
+    }
+
+	public abstract void buyHouse(Property property);
+    public abstract void buyHotel(Property property);
+    public abstract void morgage(Property property);
+    public abstract Property buyProperty(Property property);
+    public abstract Property sellProperty(Property property);
+    public abstract void sellHouse(Property property);
+    public abstract void sellHotels(Property property);
+
 
 }
