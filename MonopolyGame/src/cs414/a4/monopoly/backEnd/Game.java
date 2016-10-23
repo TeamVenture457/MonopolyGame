d18e1c015408180102f125574dd9f5f787af3e26
Jameson Mays <Jamesonmays@gmail.com>
2016-10-22 22:31:08 -0600

    Updated Board for bank instance and Game for creating players

--- MonopolyGame/src/cs414/a4/monopoly/backEnd/Game.java
+++ MonopolyGame/src/cs414/a4/monopoly/backEnd/Game.java
@@ -1,9 +1,28 @@
 package cs414.a4.monopoly.backEnd;
 
-public class Game {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
 
-	public Game() {
-		// TODO Auto-generated constructor stub
+public class Game {
+	
+	List<Player> players;
+	Board board;
+	List<String> tokens;
+
+	public Game(String [] playerNames) {
+		players = new ArrayList<Player>();
+		for(String name : playerNames){
+			players.add(new Player(name));
+		}
+		Collections.shuffle(players);
+	}
+	
+	public static void main (String [] args){
+		String [] playerNames = {"Joe", "Alex", "Sarah", "Ralph"};
+		Game game = new Game(playerNames);
 	}
 
 }
--- MonopolyGame/src/cs414/a4/monopoly/backEnd/Player.java
+++ MonopolyGame/src/cs414/a4/monopoly/backEnd/Player.java
@@ -2,7 +2,7 @@ package cs414.a4.monopoly.backEnd;
 
 public class Player extends Owner{
 	
-	private String name;
+	protected String name;
 	private String token;
 	private Space location;
 	private boolean isInJail;
