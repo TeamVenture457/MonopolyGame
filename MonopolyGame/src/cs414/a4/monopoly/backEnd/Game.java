5a8a3d47a2f4c3bf815cebe0e1900112eeb8cb06
Jameson Mays <Jamesonmays@gmail.com>
2016-10-22 23:02:21 -0600

    created functions in game to help set tokens for players using view

--- MonopolyGame/src/cs414/a4/monopoly/backEnd/Game.java
+++ MonopolyGame/src/cs414/a4/monopoly/backEnd/Game.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 
 public class Game {
 	
-	List<Player> players;
-	Board board;
+	private List<Player> players;
+	private Board board;
 	List<String> tokens;
 
 	public Game(String [] playerNames) {
@@ -18,6 +18,23 @@ public class Game {
 			players.add(new Player(name));
 		}
 		Collections.shuffle(players);
+	}
+	
+	//Gives an arraylist of player names in turn order for view to use
+	public List<String> getPlayerNamesInOrder(){
+		List<String> playerNames = new ArrayList<String>();
+		for(Player player : players){
+			playerNames.add(player.getName());
+		}
+		return playerNames;
+	}
+	
+	//takes an arraylist of tokens in player turn order from view in order to set player tokens
+	public void setPlayerTokensInOrder(List<String> playerTokens){
+		for(String token : playerTokens){
+			int index = playerTokens.indexOf(token);
+			players.get(index).setToken(token);
+		}
 	}
 	
 	public static void main (String [] args){
